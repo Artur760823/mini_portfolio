1f7e579a191c89a68856b0012a5037ca72751157
Artur <artur.zajaczkowski76@gmail.com>
2022-01-29 14:18:05 +0100

    first menu

--- safe-planet-react-page/src/App.js
+++ safe-planet-react-page/src/App.js
@@ -1,11 +1,19 @@
-
+import NavMenu from './components/NavMenu';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Router>
+        <NavMenu />
+        <Routes>
+          <Route path='/' exact />
+        </Routes>
 
-    </div>
+      </Router>
+
+    </>
   );
 }
 
---
+++ safe-planet-react-page/src/components/NavMenu.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import './NavMenu.css';
+
+
+function NavMenu() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => {
+    setClick(!click)
+  }
+
+  const closeSideMenu = () => {
+    setClick(false)
+  }
+
+  return (
+    <>
+      <nav className='menu'>
+        <div className='menu-container'>
+          <NavLink to='/' className="menu-logo">
+            Safe Our Planet <i className="fas fa-globe-americas" />
+          </NavLink>
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='menu-item'>
+              <NavLink to='/' className='menu-link' onClick={closeSideMenu}>
+                Home
+              </NavLink>
+            </li>
+            <li className='menu-item'>
+              <NavLink to='/' className='menu-link' onClick={closeSideMenu}>
+                Threats
+              </NavLink>
+            </li>
+            <li className='menu-item'>
+              <NavLink to='/' className='menu-link' onClick={closeSideMenu}>
+                Solutions
+              </NavLink>
+            </li>
+            <li className='menu-item'>
+              <NavLink to='/' className='menu-link' onClick={closeSideMenu}>
+                Activists/Organizations
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+    </>
+  );
+}
+
+export default NavMenu;
+
